e3de3bd90a6e334d67edd8af43a909ebc0d75b4c
LE ANH DAT <id96.dev@gmail.com>
2024-05-12 19:13:49 +0700

    Toan tu ++

---
+++ main.js
@@ -0,0 +1,16 @@
+// Variable definition
+let a = 1;
+let b = 1;
+
+// main
+console.log(`=== Behind ===`)
+console.log(a++);
+console.log(a);
+console.log(a++);
+console.log(a);
+
+console.log(`=== Infron ===`)
+console.log(++b);
+console.log(b);
+console.log(++b);
+console.log(b);
