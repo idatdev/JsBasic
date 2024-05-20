ecb83b1fbd281064e722131966558a92f47cec50
LE ANH DAT <id96.dev@gmail.com>
2024-05-20 22:29:31 +0700

    For and If

---
+++ main.js
@@ -0,0 +1,23 @@
+// Variable
+//let message = `Xac nhan du 18t`;
+//let isConfirm = confirm(message);
+let a = 5;
+
+// function
+
+
+// main
+for(i=0;;i++) {
+    if(a<10) {
+        console.log(a);
+        a += 1;  
+    } else {
+        break;
+    }
+}
+
+// if(isConfirm == true) {
+//     console.log(`Du tuoi`);
+// } else {
+//     console.log(`khong du tuoi`);
+// }
